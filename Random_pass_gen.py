63f117455e95c477e66ade63ea9946ae15fd1e1f
6331667 <6331667@cegepmontpetit.ca>
2025-09-28 21:02:16 -0400

    Create Random_pass_gen.py
    TODO list for what the program will do

---
+++ Random_pass_gen.py
@@ -0,0 +1,8 @@
+import random
+import os
+
+#### TODO LIST ####
+# -Capture input from user as parameters for password generation
+# -Captured parameters will be global variables (Pwd length, Symbol use, number use, Caps use)
+# -Return generated password in the console and ask to copy to clipboard 
+
