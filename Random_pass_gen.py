b2b7481c89d3c03041b55b045c6e301d2495f20c
6331667 <6331667@cegepmontpetit.ca>
2025-10-04 01:15:52 -0400

    Update Random_pass_gen.py

--- Random_pass_gen.py
+++ Random_pass_gen.py
@@ -9,7 +9,7 @@ _PWMIN = 8
 _PWMAX = 28
 
 
-_PW_LENGTH = 0
+_PW_LENGTH = 5
 _SYMBOL = False
 _NUMBER = False
 _CAPS = False
@@ -19,18 +19,33 @@ _PWORD = ""
 
 ### CALL TO GENERATE PASSWORD ###
 
-def password_generation(plength:str, psymbol:bool, pnumber:bool, pcaps:bool ):
+def password_generation(): ## params to be used are global variables that capture user entries
+
+    params = 1
+
+    if _SYMBOL:
+        params +=1
+    elif _CAPS:
+        params +=1
+    elif _NUMBER:
+        params +=1
 
 ####  get json file keys for data char arrays containing UTF-8 letters, symbols and numbers for password generation.
-
+    i = 0 ## index for pw_length
 ### init for loop pwd length if statements in said for,generate new random number at the start of the loop to get index from data
-    if _SYMBOL:
-        pass
-    elif _CAPS:
-        pass
-    elif _NUMBER:
-        pass
-        
+    while i < _PW_LENGTH:
+        rnd = random.randrange(4)
+
+        match rnd:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+
 
 # Loop while _ANSWER is true to repeat program until user selects the exit option 
 
