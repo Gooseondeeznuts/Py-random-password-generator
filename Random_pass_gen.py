7955bc43ac6f9896355f05d57920cbfeb70b24a8
Gooseondeeznuts <92948290+Gooseondeeznuts@users.noreply.github.com>
2025-10-06 21:32:50 -0400

    Update Random_pass_gen.py
    dont know how to do do randomness generation between cases so ill leave it at this for now

--- Random_pass_gen.py
+++ Random_pass_gen.py
@@ -17,34 +17,60 @@ _ANSWER = False
 
 _PWORD = ""
 
+
+
 ### CALL TO GENERATE PASSWORD ###
 
 def password_generation(): ## params to be used are global variables that capture user entries
 
-    params = 1
+    params = 1 ## default case is always going to be 1 therfore only lowercase letters will be added to the _PWORD string
 
-    if _SYMBOL:
-        params +=1
-    elif _CAPS:
-        params +=1
-    elif _NUMBER:
-        params +=1
-
+    if _CAPS:
+        params +=1  ## Second case adds CAPS letters to the _PWORD string 
+    elif _SYMBOL:
+        params +=1  ## Third case adds SYMBOLS letters to the _PWORD string
+    else:
+        params +=1  ## Fourth case adds NUMBERS letters to the _PWORD string
 ####  get json file keys for data char arrays containing UTF-8 letters, symbols and numbers for password generation.
     i = 0 ## index for pw_length
 ### init for loop pwd length if statements in said for,generate new random number at the start of the loop to get index from data
     while i < _PW_LENGTH:
-        rnd = random.randrange(params)
+        rnd = random.randrange(params) ## for randomness between cases
 
-        match rnd:
-            case 1:
+        match _CAPS:
+            case True:
                 pass
-            case 2:
+            case False:
                 pass
-            case 3:
+        
+        match _SYMBOL:
+            case True:
                 pass
-            case 4:
+            case False:
                 pass
+        
+        match _NUMBER:
+            case True:
+                pass
+            case False:
+                pass
+
+### START REGION ###
+
+def get_letters():
+    
+    pass
+
+def get_symbols():
+    
+    pass
+
+def get_nums():
+    
+    pass
+
+### END REGION ###
+
 
 
 # Loop while _ANSWER is true to repeat program until user selects the exit option 
@@ -77,7 +103,7 @@ while not _ANSWER :
         _NUMBER = bool(input("    Would you like to use Numbers y/n\n   :"))
         # Param input for symbol use
         _CAPS = bool(input("    Would you like to use CAPS y/n\n   :"))
-        password_generation(_PW_LENGTH, _SYMBOL, _NUMBER, _CAPS)
+        password_generation()
     else: break
 
 exit()
